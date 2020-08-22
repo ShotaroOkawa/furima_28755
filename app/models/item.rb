53890ab5008159a92705a892e63f57a209535375
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-23 08:09:01 +0900

    Active Hash　catogory, delv_fee, delv_from, delv_timeモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :condition
 
-  validates :category_id, numericality: { other_than: 1 } 
+  validates :category_id, :condition_id, :delv_fee_id, :delv_from_id, :delv_time_id, numericality: { other_than: 1 } 
 end
---
+++ spec/factories/delv_froms.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :delv_from do
+    
+  end
+end
