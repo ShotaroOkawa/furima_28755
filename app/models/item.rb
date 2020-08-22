344cd47914be6d0d234bfe6506dcbb167c52af57
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-23 08:16:27 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
+  has_one_attached :image
 
   validates :category_id, :condition_id, :delv_fee_id, :delv_from_id, :delv_time_id, numericality: { other_than: 1 } 
 end
