c9e5a3b9d04c6dd3e63fb7c264f8239e646e431b
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-23 14:00:47 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   has_one_attached :image
 
-  validates :category_id, :condition_id, :delv_fee_id, :delv_from_id, :delv_time_id, numericality: { other_than: 1 } 
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delv_fee_id
+    validates :delv_from_id
+    validates :delv_time_id
+  end
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }  # 価格が¥300~¥9,999,999であるか
 end
