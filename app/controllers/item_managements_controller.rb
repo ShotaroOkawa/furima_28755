f6c886df4ce3e9f7d908def6642edaa07821e8ff
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-26 08:38:06 +0900

    payjp用JSとコントローラー設定

--- app/controllers/item_managements_controller.rb
+++ app/controllers/item_managements_controller.rb
@@ -3,7 +3,30 @@ class ItemManagementsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
   def create
-  
+    @item_management = ItemManagement.new(item_id: item_management_params[:item_id], user_id: current_user.id)
+    if @item_management.valid?
+      pay_item
+      @item_management.save
+      return redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+  def item_management_params
+    params.permit(:price, :token, :item_id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: item_management_params[:price],
+      card: item_management_params[:token], 
+      currency:'jpy'
+    )
   end
 end
