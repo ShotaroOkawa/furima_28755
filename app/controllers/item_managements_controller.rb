ade86115950cdb5e5c51d936ec661fa0ba212a73
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-26 14:02:19 +0900

    Formオブジェクトコントローラー側の設定

--- app/controllers/item_managements_controller.rb
+++ app/controllers/item_managements_controller.rb
@@ -2,13 +2,15 @@ class ItemManagementsController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @item_management = ItemManagement.new
+    # @item_management_address = ItemManagementAddress.new(item_management_address_params)
   end
 
   def create
-    @item_management = ItemManagement.new(item_id: item_management_params[:item_id], user_id: current_user.id)
-    if @item_management.valid?
+    @item_management_address = ItemManagementAddress.new(item_management_address_params)
+    if @item_management_address.valid?
       pay_item
-      @item_management.save
+      @item_management_address.save
       return redirect_to root_path
     else
       @item = Item.find(params[:item_id])
@@ -17,15 +19,15 @@ class ItemManagementsController < ApplicationController
   end
 
   private
-  def item_management_params
-    params.permit(:price, :token, :item_id)
+  def item_management_address_params
+    params.permit(:price, :token, :item_id, :zip_code, :prefecture_id, :city, :lot_number, :building_name, :telephone ).merge(user_id: current_user.id)
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
     Payjp::Charge.create(
-      amount: item_management_params[:price],
-      card: item_management_params[:token], 
+      amount: item_management_address_params[:price],
+      card: item_management_address_params[:token], 
       currency:'jpy'
     )
   end
