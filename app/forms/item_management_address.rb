a767dd36954c5c723019a3b4e91e25da82bbd1f9
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-26 14:01:58 +0900

    Formオブジェクトitem_management_address作成

---
+++ app/forms/item_management_address.rb
@@ -0,0 +1,22 @@
+class ItemManagementAddress
+
+  include ActiveModel::Model
+  attr_accessor :price, :token, :item_id, :zip_code, :prefecture_id, :city, :lot_number, :building_name, :telephone, :user_id
+
+  with_options presence: true do
+    validates :zip_code
+    validates :city
+    validates :lot_number
+    validates :telephone
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :zip_code, format: { with: /\A\d{3}\-\d{4}\z/ }
+  validates :telephone, format: { with:  /\A\d{1,11}\z/ }
+
+  def save
+    item_management = ItemManagement.create(item_id: item_id, user_id: user_id)
+    Address.create(zip_code: zip_code, prefecture_id: prefecture_id, city: city, lot_number: lot_number, building_name: building_name, telephone: telephone, item_management_id: item_management.id)
+  end
+
+end
